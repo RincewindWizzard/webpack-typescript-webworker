0866b3f0fda096cf09bae871e5a323d663f6adad
RincewindWizzard <git@magierdinge.de>
2023-10-31 09:46:54 +0100

    Initial Template or Webpack Projects

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+
+const compile_stage = require('./src/compile_stage/index.ts')
+
+
+const mode = process.env.NODE_ENV || 'development';
+
+module.exports = {
+    mode,
+    entry: './src/index.ts',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.s[ca]ss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.hbs$/,
+                use: 'handlebars-loader',
+            }
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/templates/index.hbs',
+            filename: 'index.html',
+            templateParameters: compile_stage.template_context
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'css/style.css',
+        }),
+    ],
+    resolve: {
+        extensions: ['.js', '.js'],
+    },
+    devServer: {
+        static: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 8080,
+    },
+};
